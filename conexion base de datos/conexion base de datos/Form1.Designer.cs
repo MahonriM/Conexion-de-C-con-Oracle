11ab579bac8bf79121cceef940ba3b8d40d7a057
MahonriM <cripto3000z@gmail.com>
2019-05-29 23:52:43 -0500

    Add files via upload

---
+++ conexion base de datos/conexion base de datos/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace conexion_base_de_datos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnconectar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtidmaterial = new System.Windows.Forms.TextBox();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnconectar
+            // 
+            this.btnconectar.Location = new System.Drawing.Point(30, 30);
+            this.btnconectar.Name = "btnconectar";
+            this.btnconectar.Size = new System.Drawing.Size(79, 28);
+            this.btnconectar.TabIndex = 0;
+            this.btnconectar.Text = "Conectar";
+            this.btnconectar.UseVisualStyleBackColor = true;
+            this.btnconectar.Click += new System.EventHandler(this.btnconectar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "idmaterial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "descripcion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "precio";
+            // 
+            // txtidmaterial
+            // 
+            this.txtidmaterial.Location = new System.Drawing.Point(80, 122);
+            this.txtidmaterial.Name = "txtidmaterial";
+            this.txtidmaterial.Size = new System.Drawing.Size(100, 20);
+            this.txtidmaterial.TabIndex = 5;
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(80, 148);
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtdescripcion.TabIndex = 6;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(80, 172);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(100, 20);
+            this.txtprecio.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 347);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.txtdescripcion);
+            this.Controls.Add(this.txtidmaterial);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnconectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnconectar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtidmaterial;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.TextBox txtprecio;
+    }
+}
+
---
+++ conexion base de datos/conexion base de datos/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OracleClient;
+
+namespace conexion_base_de_datos
+{
+    public partial class Form1 : Form
+    {
+        OracleConnection ora = new OracleConnection("Data source=xe;user id=prue;password=p123");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnconectar_Click(object sender, EventArgs e)
+        {
+            ora.Open();
+            MessageBox.Show("Estas Conectado");
+            ora.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ora.Open();
+                OracleCommand comando = new OracleCommand("spAdd", ora);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.Add("pidmaterial", OracleType.Number).Value = txtidmaterial.Text;
+                comando.Parameters.Add("pdescripcion", OracleType.VarChar).Value = txtdescripcion.Text;
+                comando.Parameters.Add("pprecio", OracleType.Number).Value = txtprecio.Text;
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Registro Insertado");
+                ora.Close();
+
+            }
+            catch (Exception E)
+            {
+
+                MessageBox.Show("ha ocurrido un erroe"+E);
+            }
+           
+        }
+    }
+}
